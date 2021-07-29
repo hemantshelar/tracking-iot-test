954f718b1fc7996d15e51e09386352b7ef984c35
Shelar Hemant <hshelar@tcs.woolworths.com.au>
2021-07-29 11:51:06 +1000

    Intermidiate checkin

--- 4_watershade.py
+++ 4_watershade.py
@@ -7,14 +7,14 @@ import numpy as np
 from matplotlib import cm
 
 def FindCounters(img):
-	c,h= cv2.findContours(img,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
-	return c;
+	c,h= cv2.findContours(img,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+	return c
 
 def mouseCallBack(event,x,y,flags,param):
 	#print('mouse down')
 	global marks_updated
 	if event == cv2.EVENT_RBUTTONDBLCLK:
-		mouseClicked = True;
+		mouseClicked = True
 	if event == cv2.EVENT_LBUTTONDOWN:
 		#MARKERS PASSED TO THE WATERHED ALGO
 		cv2.circle(marker_image,(x,y),10,current_marker,-1)
@@ -28,7 +28,7 @@ def Create_Rgb(index):
 	x = np.array(x)
 	x = x * 255
 	x = tuple(x)
-	return x;
+	return x
 
 
 #original = cv2.imread('./imgs/water-bucket.jpg')
@@ -62,7 +62,7 @@ while True:
 
 	keyPressed = cv2.waitKey(1)
 	if keyPressed == ord('q'):
-		break;
+		break
 	elif keyPressed == ord('c'):
 		original_copy = original.copy();
 		marker_image = np.zeros(original.shape[:2],np.uint32)
@@ -85,21 +85,36 @@ segments[segments != 0] = 255
 #segments[marker_image_copy != colors[1]] = 0
 
 gray = cv2.cvtColor(segments,cv2.COLOR_BGR2GRAY);
-ret,thresh1 = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+ret,thresh1 = cv2.threshold(gray,0,255,cv2.THRESH_OTSU + cv2.THRESH_BINARY)
 
-kernal = np.ones((3,3),np.uint8)
-dil = cv2.dilate(thresh1,kernel=kernal,iterations=2)
+kernal = np.ones((5,5),np.uint8)
+erd = cv2.erode(thresh1,kernel=kernal)
+dil = cv2.dilate(erd,kernel=kernal,iterations=2)
+
+cv2.imshow('Dilated Image',dil)
 
 result = FindCounters(dil);
-print(len(result));
-cv2.drawContours(original,result,None,(0,255,255),4)
 
-cv2.imshow('Produce Level',original);
+
+for cnt in result:
+	area = cv2.contourArea(cnt)
+	if area < 10000:
+		cv2.drawContours(original,[cnt],None,(0,255,255),3)
+	else:
+		cv2.drawContours(original,[cnt],None,(0,0,255),3)
+
+		
+
+#cv2.drawContours(original,result,None,(0,255,255),4)
+
+msg = f'{len(result)}'
+cv2.imshow(msg,original);
+
+pltHelper.addImage(original_copy)
+#pltHelper.addImage(marker_image)
+#pltHelper.addImage(segments)
+pltHelper.showImages(2)
 
 cv2.waitKey()
 
-#pltHelper.addImage(original_copy)
-#pltHelper.addImage(marker_image)
-#pltHelper.addImage(segments)
-#pltHelper.showImages(2)
 cv2.destroyAllWindows();
